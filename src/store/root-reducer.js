a7c35d0e1b13001356ec45e7f2a8ca7e0a695da0
Artem Tyshchuk <artemtyschukk@gmail.com>
2023-09-25 22:31:49 +0200

    added countries to store

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { themeReducer } from "./theme/theme-reducer";
+import { countriesReducer } from "./countries/countries-reducer";
 
 export const rootReducer = combineReducers({
   theme: themeReducer,
+  countries: countriesReducer,
 });
